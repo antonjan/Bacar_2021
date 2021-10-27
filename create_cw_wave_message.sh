bdaa284d0c90eea64a290a87410ef72ce0b1ddc5
Anton <antonjan@users.noreply.github.com>
2021-10-27 12:41:22 +0200

    adding sh directory files

---
+++ create_cw_wave_message.sh
@@ -0,0 +1,1 @@
+sudo timeout 10s sudo cw -s a  -t 1000 -v 50 -f cw_text.txt| >cw.wav
---
+++ send_aprs_to_loopback.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo modprobe snd-aloop
+#/home/pi/pySSTV/create_img1.sh
+#sleep 3
+/home/pi/pySSTV/create_img2.sh
+#sleep 3
+#/home/pi/pySSTV/create_img3.sh
+#sleep 3
+while :
+do
+	/home/pi/pySSTV/send_sstv.sh&
+	aplay -D plughw:2,0 ./aprs_tes.wav
+	sleep 3
+	aplay -D plughw:2,0 ./cw.wav
+	sleep 3
+done
